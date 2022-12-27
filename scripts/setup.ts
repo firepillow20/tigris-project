ba3f6255798bc6e596234d0a33d61b6787d4648e
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2022-12-27 20:51:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ scripts/setup.ts
@@ -0,0 +1,20 @@
+import { Tigris } from '@tigrisdata/core';
+import { loadEnvConfig } from '@next/env';
+import { TodoItem } from '../db/models/todoItems';
+
+// Run the config loader only when not executing within next runtime
+if (process.env.NODE_ENV === undefined) {
+  if (process.env.APP_ENV === 'development') {
+    loadEnvConfig(process.cwd(), true);
+  } else if (process.env.APP_ENV === 'production') {
+    loadEnvConfig(process.cwd());
+  }
+}
+
+async function main() {
+  // setup client and register schemas
+  const tigrisClient = new Tigris();
+  await tigrisClient.registerSchemas([TodoItem]);
+}
+
+main();
