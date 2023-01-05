c584075c0a2e640f927a9e9bd1f7be5a7db93420
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-01-04 16:20:38 -0800

    Revert "Update layout.tsx"
    This reverts commit 7d0e7a7d06950766f9e611bf5b25ef838e8cee36.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={rubik.className}>
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <title>Neverenders Database</title>
+        <title>Todo App using Next.js 13 + Tigris</title>
         <meta name="description" content="Tigris app tutorial" />
       </head>
       <body>{children}</body>
