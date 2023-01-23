04b357dfbcf02a8c2ee3e767e7e5dc5996fb567e
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-01-22 17:28:51 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={rubik.className}>
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <title>Abababababa Database</title>
+        <title>Neverenders Database</title>
         <meta name="description" content="Tigris app tutorial" />
       </head>
       <body>{children}</body>
