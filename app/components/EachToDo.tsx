8fc60997c897700fcba4288139274302ad2442de
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-03-05 19:17:46 -0800

    Update EachToDo.tsx

--- app/components/EachToDo.tsx
+++ app/components/EachToDo.tsx
@@ -9,8 +9,9 @@ type Props = {
   deleteHandler: (id?: number) => void;
   updateHandler: (item: TodoItem) => void;
   artUpdateHandler: (item: TodoItem) => void;
-  
-  : (item: TodoItem) => void;
+  archetypeHandler: (item: TodoItem) => void;
+  healthHandler: (item: TodoItem) => void;
+  attackHandler: (item: TodoItem) => void;
 };
 const EachTodo = ({ toDoItem, deleteHandler, updateHandler, archetypeHandler, healthHandler, attackHealth artUpdateHandler }: Props) => {
   return (
