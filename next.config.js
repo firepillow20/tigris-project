0a48570da6cf195bffa801a71f77f630a16ef6ea
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-04-02 22:33:43 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,17 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
-  },
-}
-module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'media.discordapp.net',
-                port: '',
-                pathname: '/attachments/**',
-            },
-        ],
     },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'media.discordapp.net',
+            port: '',
+            pathname: '/attachments/**',
+        },
+    ],
+},
 }
 
 module.exports = nextConfig
