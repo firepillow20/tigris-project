98499c1e5ff28b82410a8a1f1865668db20692da
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-04-02 23:32:50 -0700

    Update Home.tsx

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -83,7 +83,9 @@ const Home = () => {
     fetch('/api/items', {
       method: 'POST',
         body: JSON.stringify({
-            text: "Name: " + textInput + " | Archetype: " + archetypeInput + " | Stats: " + attackInput + "/" + healthInput, completed: false, archetype: archetypeInput })
+            url: urlInput,
+            text: "Name: " + textInput + " | Archetype: " + archetypeInput + " | Stats: " + attackInput + "/" + healthInput, completed: false, archetype: archetypeInput
+        })
     }).then(() => {
       setIsLoading(false);
       setTextInput('');
