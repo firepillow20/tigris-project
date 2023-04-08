e0d8f3ed1ae93375b39a7bee7090dc6a1807f517
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-04-08 10:06:19 -0700

    Update EachToDo.tsx

--- app/components/EachToDo.tsx
+++ app/components/EachToDo.tsx
@@ -44,6 +44,7 @@ const EachTodo = ({ CardItem, deleteHandler, updateHandler, archetypeHandler, he
             height={20}
             alt="Check Image"
           />
+        </button>
         <button
           className="deleteBtn"
           onClick={() => {
