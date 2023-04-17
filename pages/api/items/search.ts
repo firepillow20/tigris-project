cf8f6d9d772b0b5f8021cc30fd9142f29e742fdf
firepillow20 <70992021+firepillow20@users.noreply.github.com>
2023-04-16 20:10:57 -0700

    a

--- pages/api/items/search.ts
+++ pages/api/items/search.ts
@@ -1,31 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { CardItem } from '../../../db/models/cardItems';
-import { SearchRequest } from '@tigrisdata/core/dist/search/types';
 import tigrisDb from '../../../lib/tigris';
 
 type Data = {
-  result?: Array<CardItem>;
-  error?: string;
+    result?: Array<CardItem>;
+    error?: string;
 };
 
 // GET /api/items/search?q=searchQ -- searches for items matching text `searchQ`
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const query = req.query['q'];
-  if (query === undefined) {
-    res.status(400).json({ error: 'No search query found in request' });
-    return;
-  }
-  try {
-    const itemsCollection = tigrisDb.getCollection<CardItem>(CardItem);
-    const searchRequest: SearchRequest<CardItem> = { q: query as string };
-    const searchResult = await itemsCollection.search(searchRequest);
-    const items = new Array<CardItem>();
-    for (const hit of searchResult.hits) {
-      items.push(hit.document);
+    const query = req.query['q'];
+    if (query === undefined) {
+        res.status(400).json({ error: 'No search query found in request' });
+        return;
     }
-    res.status(200).json({ result: items });
-  } catch (err) {
-    const error = err as Error;
-    res.status(500).json({ error: error.message });
-  }
+    try {
+        const itemsCollection = tigrisDb.getCollection<CardItem>(CardItem);
+        const searchResult = await itemsCollection.search({ q: query as string });
+        const items = new Array<CardItem>();
+        for await (const res of searchResult) {
+            res.hits.forEach(hit => items.push(hit.document));
+        }
+        res.status(200).json({ result: items });
+    } catch (err) {
+        const error = err as Error;
+        res.status(500).json({ error: error.message });
+    }
 }
